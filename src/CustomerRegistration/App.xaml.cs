cfaa407b2c172fec4dd60dc046a6b02e0da25791
Will Brigida <will_brigida@hotmail.com>
2024-12-20 09:30:58 -0300

    Iniciando desenvolvimento do teste tecnico

---
+++ src/CustomerRegistration/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace CustomerRegistration;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/CustomerRegistration/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomerRegistration;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
